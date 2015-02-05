36d07daadd8e403c470233cd51f54c2a27239e9e
Michael Dowling <mtdowling@gmail.com>
2015-02-04 20:23:08 -0800

    Updating docs and doc generator

--- src/AwsClient.php
+++ src/AwsClient.php
@@ -61,6 +61,70 @@ class AwsClient extends AbstractClient implements AwsClientInterface
     /**
      * The client constructor accepts the following options:
      *
+     * - api_provider: (callable) An optional PHP callable that accepts a
+     *   type, service, and version argument, and returns an array of
+     *   corresponding configuration data. The type value can be one of api,
+     *   waiter, or paginator.
+     * - client: (GuzzleHttp\ClientInterface|bool) Optional Guzzle client
+     *   used to transfer requests over the wire. Set to true or do not specify
+     *   a client, and the SDK will create a new client that uses a shared Ring
+     *   HTTP handler with other clients.
+     * - credentials:
+     *   (array|Aws\Credentials\CredentialsInterface|bool|callable) An
+     *   Aws\Credentials\CredentialsInterface object to use with each, an
+     *   associative array of "key", "secret", and "token" key value pairs,
+     *   `false` to utilize null credentials, or a callable credentials
+     *   provider function to create credentials using a function. If no
+     *   credentials are provided, the SDK will attempt to load them from the
+     *   environment.
+     * - debug: (bool|resource) Set to true to display debug information
+     *   when sending requests. Provide a stream resource to write debug
+     *   information to a specific resource.
+     * - defaults: (array, default=array(0)) An associative array of
+     *   default parameters to pass to each operation created by the client.
+     * - endpoint: (string) The full URI of the webservice. This is only
+     *   required when connecting to a custom endpoint (e.g., a local version
+     *   of S3).
+     * - endpoint_provider: (callable) An optional PHP callable that
+     *   accepts a hash of options including a service and region key and
+     *   returns a hash of endpoint data, of which the endpoint key is
+     *   required.
+     * - http: (array) Set to an array of Guzzle client request options
+     *   (e.g., proxy, verify, etc.). See
+     *   http://docs.guzzlephp.org/en/latest/clients.html#request-options for a
+     *   list of available options.
+     * - profile: (string) Allows you to specify which profile to use when
+     *   credentials are created from the AWS credentials file in your HOME
+     *   directory. This setting overrides the AWS_PROFILE environment
+     *   variable. Note: Specifying "profile" will cause the "credentials" key
+     *   to be ignored.
+     * - region: (string, required) Region to connect to. See
+     *   http://docs.aws.amazon.com/general/latest/gr/rande.html for a list of
+     *   available regions.
+     * - retries: (int, default=int(3)) Configures the maximum number of
+     *   allowed retries for a client (pass 0 to disable retries).
+     * - retry_logger: When the string "debug" is provided, all retries
+     *   will be logged to STDOUT. Provide a PSR-3 logger to log retries to a
+     *   specific logger instance.
+     * - ringphp_handler: (callable) RingPHP handler used to transfer HTTP
+     *   requests (see http://ringphp.readthedocs.org/en/latest/).
+     * - scheme: (string, default=string(5) "https") URI scheme to use when
+     *   connecting connect.
+     * - service: (string, required) Name of the service to utilize. This
+     *   value will be supplied by default.
+     * - signature_provider: (callable) A callable that accepts a signature
+     *   version name (e.g., v4, s3), a service name, and region, and returns a
+     *   SignatureInterface object. This provider is used to create signers
+     *   utilized by the client.
+     * - signature_version: (string) A string representing a custom
+     *   signature version to use with a service (e.g., v4, s3, v2). Note that
+     *   per/operation signature version MAY override this requested signature
+     *   version.
+     * - validate: (bool, default=bool(true)) Set to false to disable
+     *   client-side parameter validation.
+     * - version: (string, required) The version of the webservice to
+     *   utilize (e.g., 2006-03-01).
+     *
      * @param array $config Client configuration options.
      *
      * @throws \InvalidArgumentException if any required options are missing
